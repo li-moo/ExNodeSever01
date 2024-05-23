0084a15f187c7d65c91fb438d665e4eb83b7f1a9
RIMRIM <96475505+li-moo@users.noreply.github.com>
2024-05-23 21:46:58 +0900

    FEAT /parking

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ const corsOptions = {
     credentials: true
 }
 
-
-
 app.use(bodyParser.json());
 
 app.use(cors(corsOptions));
@@ -32,50 +30,6 @@ app.use(cors(corsOptions));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 }) //포트 4000번에서 이 앱을 실행한다.
-
-////// 여기서 부터 찐 API
-// app.get("/member", (req, res) => {
-//     const memberNumber = req.query.member_number; // member_number은 db에서 memberNumber은 내가 서버단에서 지은 이름
-//     db.query('SELECT * FROM member WHERE member_number = ?', [memberNumber], (err, result) => {
-//         if (err) {
-//             console.error('member data error:', err);
-//             res.status(500).send('내부 서버 오류');
-//         } else {
-//             if (result.length === 0) {
-//                 res.status(404).send('해당하는 회원이 없습니다.');
-//             } else {
-//                 res.json(result[0]); // 결과가 배열 형태로 반환되므로 첫 번째 요소만 반환
-//                 console.log('member data:', result[0]);
-//             }
-//         }
-//     });
-//   });
-// app.get("/member", (req, res) => {
-//     const memberNumber = req.query.member_number;
-
-//     db.query(`
-//         SELECT member.name, member.member_number, member.member_img, member.car_number, 
-//                membership_period.first_membership_period, membership_period.second_membership_period,
-//                parking.is_parking
-//         FROM member
-//         LEFT JOIN membership_list ON member.member_number = membership_list.member_number
-//         LEFT JOIN membership_period ON membership_list.membership_id = membership_period.membership_id
-//         LEFT JOIN parking ON member.car_number = parking.car_number
-//         WHERE member.member_number = ?`, [memberNumber], (err, result) => {
-//         if (err) {
-//             console.error('member data error:', err);
-//             res.status(500).send('내부 서버 오류');
-//         } else {
-//             if (result.length === 0) {
-//                 res.status(404).send('해당하는 회원이 없습니다.');
-//             } else {
-//                 res.json(result[0]);
-//                 console.log('member data:', result[0]);
-//             }
-//         }
-//     });
-// });
-
 
 app.get("/member", (req, res) => {
     const memberNumber = req.query.member_number;
@@ -106,66 +60,16 @@ app.get("/member", (req, res) => {
 });
 
 
-// app.post('/attendance', (req, res) => {
-//     const { memberNumber, inTime, outTime, inProgress, isParking, parkingTime } = req.body;
-
-//     // 오늘 날짜 생성 (YYYY-MM-DD 형식)
-//     const today = new Date().toISOString().split('T')[0];
-
-//     const query = `
-//         INSERT INTO attendance (member_number, today, in_time, out_time, in_progress, is_parking) 
-//         VALUES (?, ?, ?, ?, ?, ?);
-        
-//         INSERT INTO parking (car_number, out_time, parking_time, is_parking) 
-//         VALUES (?, ?, ?, ?);`;
-
-//     const values = [
-//         memberNumber, today, inTime, outTime, inProgress, isParking,  // values for attendance table
-//         memberNumber, outTime, parkingTime, isParking                 // values for parking table
-//     ];
-
-//     db.query(query, values, (err, result) => {
-//         if (err) {
-//             console.error('Insert error:', err);
-//             res.status(500).send('내부 서버 오류');
-//             return;
-//         }
-
-//         res.status(200).send('Data inserted successfully');
-//     });
-// });
-
-// app.post('/attendance', (req, res) => {
-//     const { member_number } = req.body;
-//     const today = new Date().toISOString().slice(0, 10); // YY-MM-DD 형식의 오늘 날짜
-//     const in_time = new Date().toISOString().slice(11, 19); // HH:MM:SS 형식의 현재 시간
-//     const in_progress = true;
-//     const query = `
-//         INSERT INTO attendance (member_number, today, in_time in_progress)
-//         VALUES (?, ?, ?, ?, ?)
-//     `;
-
-//     db.query(query, [member_number, today, in_time, in_progress], (err, result) => {
-//         if (err) {
-//             console.error('데이터 삽입 중 오류 발생:', err);
-//             res.status(500).json({ error: '데이터 삽입 중 오류 발생' });
-//         } else {
-//             res.status(201).json({ id: result.insertId, message: '데이터 삽입 성공' });
-//         }
-//     });
-// });
 app.post('/attendance', (req, res) => {
     const { member_number } = req.body;
     const today = new Date().toISOString().slice(0, 10); // YY-MM-DD 형식의 오늘 날짜
     const in_time = new Date().toISOString().slice(11, 19); // HH:MM:SS 형식의 현재 시간
-    const out_time = null; // 아직 출석 종료 시간을 모릅니다.
-    const in_progress = true; // 출석 중 상태
-
+    const out_time = null; // 아직 출석 종료 시간을 모름 null.
+    const in_progress = true; // 정기권이 진행 중인가
     const query = `
         INSERT INTO attendance (member_number, today, in_time, out_time, in_progress)
         VALUES (?, ?, ?, ?, ?)
     `;
-
     db.query(query, [member_number, today, in_time, out_time, in_progress], (err, result) => {
         if (err) {
             console.error('데이터 삽입 중 오류 발생:', err);
@@ -175,6 +79,63 @@ app.post('/attendance', (req, res) => {
         }
     });
 });
+
+app.post('/parking', (req, res) => {
+    const { car_number, member_number, out_time, parking_time } = req.body;
+    const parkingQuery = `
+        INSERT INTO parking (car_number, member_number, out_time, parking_time)
+        VALUES (?, ?, ?, ?)
+    `;
+
+    const attendanceQuery = `
+    UPDATE attendance
+    SET out_time = ?
+    WHERE member_number = ?
+    AND out_time IS NULL
+    ORDER BY id DESC LIMIT 1;
+    `;
+
+    // 트랜잭션
+    db.beginTransaction((err) => {
+        if (err) {
+        return res.status(500).json({ error: '트랜잭션 시작 중 오류 발생' });
+        }
+
+        db.query(parkingQuery, [car_number, member_number, out_time, parking_time], (err, result) => {
+        if (err) {
+            return db.rollback(() => {
+            console.error('parking 테이블에 데이터 삽입 중 오류 발생:', err);
+            res.status(500).json({ error: 'parking 테이블에 데이터 삽입 중 오류 ' });
+            });
+        } // 만약 parkingQuery만 실행되고 attanceQuery가 실행안된다하더라도
+            // 롤백을 잘못된 데이터가 저장되지 않는다.
+
+            // [out_time, member_number] 둘의 순서가 봐꼈을 때, 오류는 안났지만 데이터가 들어가진않았다
+            // .set으로 out_time
+        db.query(attendanceQuery, [out_time, member_number], (err, result) => {
+            if (err) {
+            return db.rollback(() => {
+                console.error('attendance 테이블에 데이터 삽입 중 오류 발생:', err);
+                res.status(500).json({ error: 'attendance 테이블에 데이터 삽입 중 오류' });
+                console.log(result);
+            });
+            }
+
+            // 트랜잭션 커밋
+            db.commit((err) => {
+            if (err) {
+                return db.rollback(() => {
+                console.error('트랜잭션 커밋 중 오류 발생:', err);
+                res.status(500).json({ error: '트랜잭션 커밋 중 오류 발생' });
+                });
+            }
+            res.status(201).json({ message: '데이터 삽입 성공' });
+            });
+        });
+        });
+    });
+});
+
 
 
 // 실행 node.index.js -> 서버 실행
